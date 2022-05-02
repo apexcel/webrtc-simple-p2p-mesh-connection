ca0ed542e15abb4d9c1d0423b097d99a42803614
apexcel <dev.apexcel@gmail.com>
2022-05-02 20:48:11 +0900

    Fix socket url

--- packages/client/src/lib/socket.ts
+++ packages/client/src/lib/socket.ts
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-const url = 'ws://localhost:5000/';
+const dev = 'ws://localhost:5000/';
+const prod = 'ws://webrtc-simple-p2p-mesh.herokuapp.com/'
+
+const url = prod;
 const socket = io(url);
 
 export default socket;
